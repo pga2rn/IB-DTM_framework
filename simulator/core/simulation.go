aa8df604142bb27bbc8f26e4cb671c058e99e844
pga2rn <aaronpigybd@gmail.com>
2020-12-16 18:06:19 +0900

    Finish vehicle moving logic.

--- simulator/core/simulation.go
+++ simulator/core/simulation.go
@@ -3,6 +3,7 @@ package core
 import (
 	"context"
 	"errors"
+	"github.com/pga2rn/ib-dtm_framework/shared/logutil"
 	"github.com/pga2rn/ib-dtm_framework/simulator/dtm"
 	"github.com/pga2rn/ib-dtm_framework/simulator/vehicle"
 	"time"
@@ -73,6 +74,8 @@ func (sim *SimulationSession) WaitForVehiclesInit() error {
 func (sim *SimulationSession) InitVehicles() bool {
 	// activate the very first VehiclesNumMin vehicles
 	sim.ActiveVehiclesNum = sim.Config.VehicleNumMin
+
+	// init activated vehicles
 	for i := 0 ; i < int(sim.Config.VehicleNumMin); i++ {
 		v := &vehicle.Vehicle{}
 		v.Pos = vehicle.Position {
@@ -90,6 +93,18 @@ func (sim *SimulationSession) InitVehicles() bool {
 		// place the vehicle onto the map
 		sim.Map.Cross[v.Pos.X][v.Pos.Y].Vehicles[uint64(i)] = v
 	}
+
+	// init inactivate vehicles
+	for i := sim.Config.VehicleNumMin; i < sim.Config.VehicleNumMax; i++ {
+		v := &vehicle.Vehicle{}
+		v.VehicleStatus = vehicle.InActive
+		v.LastMovementDirection = vehicle.NotMove
+
+		// register the vehicle to the session
+		sim.Vehicles[i] = v
+		sim.ActiveVehiclesBitMap.Set(int(i), false)
+	}
+
 	// init all vehicles' trust value
 	sim.AccurateTrustValueList = make([]float32, sim.Config.VehicleNumMax, 1.0)
 	sim.BiasedTrustValueList = make([]float32, sim.Config.VehicleNumMax, 1.0)
@@ -98,11 +113,41 @@ func (sim *SimulationSession) InitVehicles() bool {
 }
 
 ////// simulation routines //////
+// process slot
+// routine:
+// 1. move vehicles(including moving out of the map, set as inactive)
+// 1.1 check if nums of vehicles less than vehiclenummin, or activate new vehicles
+// 1.2 update rsu vehicles list
+// 2. generate trust value offsets for every active vehicles
+// 3. update rsu
+// 3.1 update rsu trustvalueoffsetlist
+func (sim *SimulationSession) ProcessSlot(ctx context.Context, slot uint64) error{
+	SlotCtx, cancel := context.WithCancel(ctx)
+	c := make(chan interface{})
+
+	// move the vehicles!
+	go sim.moveVehicles(SlotCtx, c)
+	// update rsu trust value offset list
+	go sim.executeRSU(SlotCtx, c)
+
+	select {
+	case <-ctx.Done():
+		logutil.LoggerList["core"].Debugf("[ProcessSlot] context canceled.")
+		cancel()
+		return errors.New("context canceled")
+	case <- c:
+		// the 2 go routines finished
+		return nil
+	}
+}
+
+
+
 // process epoch
 // routine:
-// 1. move the vehicles
-//	1.0 check if the activated vehicles are enough, or
-// 1.1 generate trust value offsets for every active vehicles
+// 1. process slot
+// 2. calculated trust value and stored in the session
+// 3. statistics gathering
 func (sim *SimulationSession) ProcessEpoch(ctx context.Context, slot uint64){
 
 }
